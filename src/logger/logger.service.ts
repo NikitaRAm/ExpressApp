b95ba0bfd503060e8681febb38d7fca74dcd6549
nikita_romanovich1@bat.com <nikita_romanovich1@bat.com>
2022-11-29 15:42:07 +0300

    added eslint and prettier

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,30 +1,29 @@
 import { injectable } from 'inversify';
-import { Logger } from 'tslog'
+import { Logger } from 'tslog';
 import { ILogger } from './logger.interface';
 import 'reflect-metadata';
 
 @injectable()
 export class LoggerService implements ILogger {
-    public logger:Logger;
+	public logger: Logger;
 
-    constructor() {
-        this.logger = new Logger({
-            displayInstanceName: false,
-            displayLoggerName: false,
-            displayFilePath: 'hidden',
-            displayFunctionName: false
-        });
-    }
+	constructor() {
+		this.logger = new Logger({
+			displayInstanceName: false,
+			displayLoggerName: false,
+			displayFilePath: 'hidden',
+			displayFunctionName: false,
+		});
+	}
 
-    log(...arg:unknown[]) {
-        this.logger.info(...arg)
-    }
+	log(...arg: unknown[]): void {
+		this.logger.info(...arg);
+	}
 
-    error(...arg:unknown[]) {
-        this.logger.error(...arg)
-    }
-    warn(...arg:unknown[]) {
-        this.logger.warn(...arg)
-    }
-
+	error(...arg: unknown[]): void {
+		this.logger.error(...arg);
+	}
+	warn(...arg: unknown[]): void {
+		this.logger.warn(...arg);
+	}
 }
