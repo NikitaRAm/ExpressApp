c1349a2d65343af097ce81c6818a6b84a032416f
nikita_romanovich1@bat.com <nikita_romanovich1@bat.com>
2023-03-09 16:53:31 +0300

    added middleware and validator

--- src/users/dto/user-login.dto.ts
+++ src/users/dto/user-login.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsString({ message: 'no email specified' })
 	email: string;
+	@IsString({ message: 'no password specified' })
 	password: string;
+	@IsString({ message: 'no name specified' })
 	name: string;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,7 +9,7 @@ export class UserService implements IUserService {
 	async createUser({ email, name, password }: UserRegisterDto): Promise<User | null> {
 		const newUser = new User(email, name);
 		await newUser.setPassword(password, 5);
-		return null;
+		return newUser;
 	}
 
 	async validateUser(dto: UserLoginDto): Promise<boolean> {
